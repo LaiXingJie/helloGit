63a666712f456207433670ce5e637f7dd4d64775
Think <185752822@qq.com>
2020-06-15 13:40:00 +0800

    更新1

--- src/main/java/com/guigu/git/HelloGit.java
+++ src/main/java/com/guigu/git/HelloGit.java
@@ -2,11 +2,12 @@ package com.guigu.git;
 
 /**
  * @author think
- * @Date 2020/6/15
+ *
  */
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("更新1");
     }
 }
 
