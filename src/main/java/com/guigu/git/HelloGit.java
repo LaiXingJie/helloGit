6d58aebae553b0b10fcca1f498a6c2552eead037
Think <185752822@qq.com>
2020-06-15 13:31:07 +0800

    测试提交

---
+++ src/main/java/com/guigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.guigu.git;
+
+/**
+ * @author think
+ * @Date 2020/6/15
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
+
